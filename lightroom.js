510b5863ae9bff1a136fb6d1976a21d98cead719
BugHub <allin.schroe@gmail.com>
2019-11-03 22:25:21 +0800

    lightroom

--- lightroom.js
+++
@@ -1,5 +0,0 @@
-#lightroom
-^https:\/\/photos\.adobe\.io\/v2\/accounts* url response-body “status”:.* response-body “status”:”subscriber”,”current_subs”:{“product_id”:”lightroom”,”store”:”adobe”,”purchase_date”:”2019-10-10T16:32:10.254954Z”,”sao”:{“inpkg_CCES”:”0”,”inpkg_CCLE”:”1”,”inpkg_CCSN”:”0”,”inpkg_CCSV”:”0”,”inpkg_LCCC”:”0”,”inpkg_LPES”:”0”,”inpkg_LRBRL”:”0”,”inpkg_LRMAC”:”0”,”inpkg_LRMC”:”0”,”inpkg_LRMP”:”0”,”inpkg_LRTB”:”0”,”inpkg_PHLT”:”0”,”inpkg_PHLT2”:”0”,”inpkg_PLES”:”0”,”storage_quota”:”100”}},”storage”:{“used”:0,”limit”:1154487209165,”display_limit”:1099511627776,”warn”:992137445376}},”avatar”:{“placeholder”:true}}
-/**
- * @supported 29022075
- */
